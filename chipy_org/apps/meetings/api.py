bd19575900b5a081d26e948647e48aacb6f0c69f
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2013-01-23 10:07:45 -0600

    Added some other resources for api

--- chipy_org/apps/meetings/api.py
+++ chipy_org/apps/meetings/api.py
@@ -1,10 +1,20 @@
 from tastypie import fields
 from tastypie.resources import ModelResource
-from meetings.models import Meeting, Venue
+from meetings.models import Meeting, Venue, Presentor, Topic
 
 class VenueResource(ModelResource):
     class Meta:
         queryset = Venue.objects.all()
+
+
+class PresentorResource(ModelResource):
+    class Meta:
+        queryset = Presentor.objects.all()
+
+
+class TopicResource(ModelResource):
+    class Meta:
+        queryset = Topic.objects.all()
 
 
 class MeetingResource(ModelResource):
